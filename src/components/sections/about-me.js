689f2ddb309274812d4044b3c06e578b54f40140
Joe Christianson <jcfargond@gmail.com>
2022-05-22 22:44:23 -0500

    added avatar

--- src/components/sections/about-me.js
+++ src/components/sections/about-me.js
@@ -1,10 +1,15 @@
+import avatar from "../../images/Profile.png"
+import "../../styles/about-me.css"
 function AboutMe() {
     return (
-      <div>I'm Joe. Nice to meet you.
+      <div id="about-me-section">
+      <p className="h-spacer">I'm Joe. Nice to meet you.
 
       I am a multi-talented individual who is just beginning his coding journey. I have a history in both the business world and in academia that complement each other well for the creation of commercial web projects. I understand what businesses are typically looking for and speak their language well. Also, I have experience in very precise thinking that came with years of philosophy writing and argumentation.
       
-      I find joy in coding and the problem solving that comes along with it. I am voracious when it comes to learning new tools, but also learning old tools in depth.</div>
+      I find joy in coding and the problem solving that comes along with it. I am voracious when it comes to learning new tools, but also learning old tools in depth.</p>
+      <img className="h-spacer" id="avatar" src={avatar}></img>
+      </div>
     );
   }
   
