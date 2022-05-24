f0af06056392dcbf6ab39c381921cbbd03c069e3
Joe Christianson <jcfargond@gmail.com>
2022-05-24 10:00:10 -0500

    further styling

--- src/components/sections/project.js
+++ src/components/sections/project.js
@@ -7,18 +7,19 @@ function Project({project}) {
     return (
       <section>
           <section className="left">
+            <h2>{project.name}</h2>
           <img className="project-image" src={image.image}></img>
           <Tech tech={project.tech}></Tech>
+          <div className="links-container">
+            <a href={project.live}>Live Site</a>
+            <a href={project.code}>Code</a>
+          </div>
           </section>
         <section className="right">
           <h3>Summary</h3>
           <p>{project.description}</p>
           <h3>Future Development</h3>
           <p>{project.future}</p>
-          <div className="links-container">
-            <a href={project.live}>Live Site</a>
-            <a href={project.code}>Code</a>
-          </div>
         </section>
 
       </section>
--- src/data/projects.js
+++ src/data/projects.js
@@ -29,7 +29,7 @@ const sarahTurner = new Collaborator("Sarah","Turner","https://github.com/smturn
 const ethanJohnson = new Collaborator("Ethan","Johnson","https://github.com/AmplifyRebel","")
 const jacksonWilliams = new Collaborator("Jackson","Williams","https://github.com/jackbradwilliams","")
 
-const youFood = new Project("YouFood",
+const youFoods = new Project("YouFoods",
 "this",
 "youfoods.jpg",
 ["javascript","node","express","handlebars","mysql","sequelize","google-maps-api","heroku"],
@@ -38,8 +38,8 @@ const youFood = new Project("YouFood",
 "https://github.com/JoeChristianson/Food_Pantry_Map",""
 );
 
-youFood.description = `For this application, we created an online website in which users can track, contribute to, and set up various food shelves. Immediately upon loading the website, a map is generated using google maps that centers on the users location. Various cherry markers pop up that show nearby food shelters that other users have created. Clicking a marker will display information about that particular food shelf like its name, address, and the supplies that it needs. Users can choose to register and create a username and password that are stored within the application's database. They can use this login information to create a new food shelter and request supplies. A new marker will then be generated for the address that they provided. For each food shelf created, the user that created it can edit and post new requests that are then updated on their marker information.`
-youFood.future = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+youFoods.description = `For this application, we created an online website in which users can track, contribute to, and set up various food shelves. Immediately upon loading the website, a map is generated using google maps that centers on the users location. Various cherry markers pop up that show nearby food shelters that other users have created. Clicking a marker will display information about that particular food shelf like its name, address, and the supplies that it needs. Users can choose to register and create a username and password that are stored within the application's database. They can use this login information to create a new food shelter and request supplies. A new marker will then be generated for the address that they provided. For each food shelf created, the user that created it can edit and post new requests that are then updated on their marker information.`
+youFoods.future = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 
 const movieGenerator = new Project("Movie Generator","this is it","movie-generator.png",["javascript","bootstrap","html"],[sarahTurner,ethanJohnson,jacksonWilliams],"https://amplifyrebel.github.io/movie-generator/","https://github.com/AmplifyRebel/movie-generator","");
